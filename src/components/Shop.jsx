a528549f3b2ecf47e65083b87ee996354f0fc05a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-09-06 18:52:52 +0100

    Struggling with getting product quantity to work

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,6 +4,8 @@ export default function Shop() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState([]);
+  const [quantity, setQuantity] = useState(0);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -18,6 +20,29 @@ export default function Shop() {
       .finally(() => setLoading(false));
   }, []);
 
+  function addToCart(productId) {
+    if (cart.length > 0) {
+      const newCart = [...cart];
+      newCart.forEach((item, index) => {
+        if (item.id === productId && item.quantity === quantity) {
+          newCart.splice(index, 1, item);
+          setCart([...newCart]);
+        } else {
+          const cartProduct = products.find(
+            (product) => product.id === productId
+          );
+          cartProduct.quantity = quantity;
+          setCart([...cart, cartProduct]);
+        }
+      });
+    } else {
+      const cartProduct = products.find((product) => product.id === productId);
+      cartProduct.quantity = quantity;
+      setCart([...cart, cartProduct]);
+    }
+    console.log(cart);
+  }
+
   return error ? (
     <h1>Oops! There's a network error!</h1>
   ) : loading ? (
@@ -30,6 +55,21 @@ export default function Shop() {
           <h2>{product.title}</h2>
           <p>${product.price}</p>
           <p>{product.description}</p>
+          <form action="">
+            <label htmlFor="quantity">Quantity </label>
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              min={1}
+              defaultValue={1}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+            />
+          </form>
+          <h2>{product.id}</h2>
+          <button type="button" onClick={() => addToCart(product.id)}>
+            Add to Cart
+          </button>
         </article>
       ))}
     </section>
