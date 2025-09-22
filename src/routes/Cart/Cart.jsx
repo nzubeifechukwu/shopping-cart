4e496ee2e25a9d22dbdf019ab0cf1ad8e490b509
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-09-22 18:44:46 +0100

    Fix issue with cart items quantity not updating live

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link, useOutletContext } from "react-router";
 
 import shopStyles from "../Shop/shop.module.css";
@@ -7,18 +6,16 @@ import cartStyles from "./cart.module.css";
 export default function Cart() {
   const [cart, setCart] = useOutletContext();
 
-  // will toggle state to ensure rerender once increase (+) or decrease (-) button is clicked.
-  // There may be a better way to do this than setting a more or less redundant state.
-  const [rerender, setRerender] = useState(false);
-
   function increaseQuantity(id) {
-    const item = cart.find((element) => id === element.id);
+    const newCart = [...cart];
+    const item = newCart.find((element) => id === element.id);
     item.quantity += 1;
-    setRerender(!rerender);
+    setCart(newCart);
   }
 
   function decreaseQuantity(id) {
-    const item = cart.find((element) => element.id === id);
+    const newCart = [...cart];
+    const item = newCart.find((element) => element.id === id);
     if (item.quantity > 1) {
       item.quantity -= 1;
     } else {
@@ -26,7 +23,7 @@ export default function Cart() {
         "You have just 1 of this item left in your cart. Click the Remove button if you want to remove it."
       );
     }
-    setRerender(!rerender);
+    setCart(newCart);
   }
 
   return (
